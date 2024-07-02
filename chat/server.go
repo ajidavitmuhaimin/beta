9b230a9794182d1aebe3dcafe89598c1933e96ca
Aji Davit Muhaimin <103036177+ajidavitmuhaimin@users.noreply.github.com>
2024-07-03 06:43:22 +0700

    Update server.go

--- chat/server.go
+++ chat/server.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 import "net/http"
 
+
+
 func main(){
 
    http.HandleFunc("/send",sendHandler)
+   http.HandleFunc("/conn",connHandler)
    http.ListenAndServe(":1395",nil)
 
 }
